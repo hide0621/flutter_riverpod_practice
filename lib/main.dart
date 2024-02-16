798ec5b75f6e6e9fdc661bbdbae1a8c2ddbcc4dc
hide0621 <fujihara_hideyuki@retail-ai.jp>
2024-02-16 09:59:03 +0900

    StateNotifierProviderで簡単なインクリメントを実装した

--- lib/main.dart
+++ lib/main.dart
@@ -24,46 +24,47 @@ class MyApp extends StatelessWidget {
   }
 }
 
-// Providerは外部から変更することができないデータを管理するためのものであり（下のコードだとHello, Riverpod!）、
-// このデータは、アプリケーション全体で共有される
+//StateNotifierProvider で管理するStateNotifier のサブクラス（CounterNotifier）を実装する
+class CounterNotifier extends StateNotifier<int> {
+  CounterNotifier() : super(0);
 
-//Providerをグローバル変数として定義する
-//Providerとはデータを管理する入れ物のようなクラスで、文字列'Hello, Riverpod!'を保持する状態を作成している
-//この例では、String型のデータである'Hello, Riverpod!'を管理するProviderを定義している
-//Providerは、データを提供するためのものであり、データを提供するための関数をラムダ式で指定する
-//refは、他のプロバイダーを参照するために使用できるProviderReferenceオブジェクトであり、ラムダ式の引数である
-//refはラムダ式の引数（パラメータ）であり、関数本体（つまり、評価される式）は「'Hello, Riverpod!'」であり、
-//このラムダ式は呼び出されると常に「'Hello, Riverpod!'」を返す
-final strProvider = Provider((ref) => 'Hello, Riverpod!');
+  void increment() {
+    state++;
+  }
+}
 
-//Providerからデータを取得する
-// Providerからデータを取得するためにConsumerWidget を使用する
-// ConsumerWidget を継承したWidget（今回はRiverpodSample）を定義すると、build() にWidgetRef 型の引数が追加できる
+// StateNotifierProvider を作成する
+final counterProvider = StateNotifierProvider<CounterNotifier, int>((ref) {
+  return CounterNotifier();
+});
+
 class RiverpodSample extends ConsumerWidget {
-  // このコンストラクタは、RiverpodSampleという名前の新しいウィジェット（オブジェクト）を生成し、
-  // そのウィジェットはRiverpodのプロバイダーからデータを読み取ることができます。
-  // また、このウィジェットは、オプションで一意のKeyを持つことができます。
-  // Flutterフレームワークはこのkeyを使用して、ウィジェットの同一性を追跡して、一意性を保証することができる
-  // 初期化リスト「:」があるので、親クラスのコンストラクタが先に実行され、
-  // その後にこのクラスのコンストラクタが実行される
-  // 子クラスのコンストラクタに渡されたkeyは、親クラスのコンストラクタに渡されるが、
-  // 初期化の手順は親クラスのコンストラクタが先に実行される
-  // この一連の流れにより、子クラスが親クラスのメソッドやプロパティを利用することができる
   const RiverpodSample({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    // ProviderにstrProvider を指定しているので、strProvider が管理しているデータである'Hello Riverpod' を取得することができる
-    final value = ref.watch(strProvider);
+    //
+    final countStateController = ref.read(counterProvider.notifier);
+    //
+    final count = ref.watch(counterProvider);
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Riverpod Sample'),
       ),
       body: Center(
-          child: Text(
-        value,
-        style: const TextStyle(fontSize: 24),
-      )),
+        child: Text(
+          count.toString(),
+          style: const TextStyle(fontSize: 24),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          countStateController.increment();
+        },
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
     );
   }
 }
