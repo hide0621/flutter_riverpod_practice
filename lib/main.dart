d818743bbefb169ea6de384502a83607776ea3ac
hide0621 <fujihara_hideyuki@retail-ai.jp>
2024-02-16 15:48:59 +0900

    FutureProviderを使って簡単なサンプルを書いてみた

--- lib/main.dart
+++ lib/main.dart
@@ -24,46 +24,36 @@ class MyApp extends StatelessWidget {
   }
 }
 
-// Providerは外部から変更することができないデータを管理するためのものであり（下のコードだとHello, Riverpod!）、
-// このデータは、アプリケーション全体で共有される
+final futureProvider = FutureProvider<String>((ref) async {
+  await Future.delayed(const Duration(seconds: 3));
+  return 'Hello Future Riverpod!';
+});
 
-//Providerをグローバル変数として定義する
-//Providerとはデータを管理する入れ物のようなクラスで、文字列'Hello, Riverpod!'を保持する状態を作成している
-//この例では、String型のデータである'Hello, Riverpod!'を管理するProviderを定義している
-//Providerは、データを提供するためのものであり、データを提供するための関数をラムダ式で指定する
-//refは、他のプロバイダーを参照するために使用できるProviderReferenceオブジェクトであり、ラムダ式の引数である
-//refはラムダ式の引数（パラメータ）であり、関数本体（つまり、評価される式）は「'Hello, Riverpod!'」であり、
-//このラムダ式は呼び出されると常に「'Hello, Riverpod!'」を返す
-final strProvider = Provider((ref) => 'Hello, Riverpod!');
-
-//Providerからデータを取得する
-// Providerからデータを取得するためにConsumerWidget を使用する
-// ConsumerWidget を継承したWidget（今回はRiverpodSample）を定義すると、build() にWidgetRef 型の引数が追加できる
 class RiverpodSample extends ConsumerWidget {
-  // このコンストラクタは、RiverpodSampleという名前の新しいウィジェット（オブジェクト）を生成し、
-  // そのウィジェットはRiverpodのプロバイダーからデータを読み取ることができます。
-  // また、このウィジェットは、オプションで一意のKeyを持つことができます。
-  // Flutterフレームワークはこのkeyを使用して、ウィジェットの同一性を追跡して、一意性を保証することができる
-  // 初期化リスト「:」があるので、親クラスのコンストラクタが先に実行され、
-  // その後にこのクラスのコンストラクタが実行される
-  // 子クラスのコンストラクタに渡されたkeyは、親クラスのコンストラクタに渡されるが、
-  // 初期化の手順は親クラスのコンストラクタが先に実行される
-  // この一連の流れにより、子クラスが親クラスのメソッドやプロパティを利用することができる
   const RiverpodSample({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    // ProviderにstrProvider を指定しているので、strProvider が管理しているデータである'Hello Riverpod' を取得することができる
-    final value = ref.watch(strProvider);
+    final value = ref.watch(futureProvider);
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Riverpod Sample'),
       ),
       body: Center(
-          child: Text(
-        value,
-        style: const TextStyle(fontSize: 24),
-      )),
+        child: value.when(
+            data: (data) {
+              return Text(
+                data.toString(),
+                style: const TextStyle(fontSize: 24),
+              );
+            },
+            error: (error, trace) => Text(
+                  error.toString(),
+                  style: const TextStyle(fontSize: 24),
+                ),
+            loading: () => const CircularProgressIndicator()),
+      ),
     );
   }
 }
