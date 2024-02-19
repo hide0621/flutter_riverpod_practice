445f1a56862247b4d79c91a0929567fdf998d385
hide0621 <fujihara_hideyuki@retail-ai.jp>
2024-02-19 10:03:55 +0900

    ConsumerStatefulWidgetについて、その実装方法を学び、サンプルコードを書いてみた

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-// ルートにProviderScopeを追加する
-// こうすることで、アプリ全体でProviderを利用できる
-// （上位WidgetのMyApp()を囲むことで、MyApp以降のWidgetでもProviderを呼び出すことができる）
 void main() {
   runApp(ProviderScope(child: MyApp()));
 }
@@ -24,36 +21,24 @@ class MyApp extends StatelessWidget {
   }
 }
 
-// Providerは外部から変更することができないデータを管理するためのものであり（下のコードだとHello, Riverpod!）、
-// このデータは、アプリケーション全体で共有される
-
-//Providerをグローバル変数として定義する
-//Providerとはデータを管理する入れ物のようなクラスで、文字列'Hello, Riverpod!'を保持する状態を作成している
-//この例では、String型のデータである'Hello, Riverpod!'を管理するProviderを定義している
-//Providerは、データを提供するためのものであり、データを提供するための関数をラムダ式で指定する
-//refは、他のプロバイダーを参照するために使用できるProviderReferenceオブジェクトであり、ラムダ式の引数である
-//refはラムダ式の引数（パラメータ）であり、関数本体（つまり、評価される式）は「'Hello, Riverpod!'」であり、
-//このラムダ式は呼び出されると常に「'Hello, Riverpod!'」を返す
 final strProvider = Provider((ref) => 'Hello, Riverpod!');
 
-//Providerからデータを取得する
-// Providerからデータを取得するためにConsumerWidget を使用する
-// ConsumerWidget を継承したWidget（今回はRiverpodSample）を定義すると、build() にWidgetRef 型の引数が追加できる
-class RiverpodSample extends ConsumerWidget {
-  // このコンストラクタは、RiverpodSampleという名前の新しいウィジェット（オブジェクト）を生成し、
-  // そのウィジェットはRiverpodのプロバイダーからデータを読み取ることができます。
-  // また、このウィジェットは、オプションで一意のKeyを持つことができます。
-  // Flutterフレームワークはこのkeyを使用して、ウィジェットの同一性を追跡して、一意性を保証することができる
-  // 初期化リスト「:」があるので、親クラスのコンストラクタが先に実行され、
-  // その後にこのクラスのコンストラクタが実行される
-  // 子クラスのコンストラクタに渡されたkeyは、親クラスのコンストラクタに渡されるが、
-  // 初期化の手順は親クラスのコンストラクタが先に実行される
-  // この一連の流れにより、子クラスが親クラスのメソッドやプロパティを利用することができる
+// ConsumerStatefulWidgetにすることにより、
+// buildメソッドの引数にrefを渡さずに、
+// stateからrefを参照することができる（値を参照することができる）
+class RiverpodSample extends ConsumerStatefulWidget {
   const RiverpodSample({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    // ProviderにstrProvider を指定しているので、strProvider が管理しているデータである'Hello Riverpod' を取得することができる
+  ConsumerState<RiverpodSample> createState() => RiverpodSampleState();
+}
+
+class RiverpodSampleState extends ConsumerState<RiverpodSample> {
+  // 以下のbuildの中のref部分のように、
+  // buildメソッドの引数にrefを渡さずに、
+  // stateからrefを参照することができる（値を参照することができる）
+  @override
+  Widget build(BuildContext context) {
     final value = ref.watch(strProvider);
     return Scaffold(
       appBar: AppBar(
